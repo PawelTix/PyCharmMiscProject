c83c5e17221c1486f165e7892e944ff8bd0d8146
PawelTix <pawel.rzodkiewicz@kowale.edu.pl>
2025-11-15 10:56:52 +0100

    Losowe generatory i powerplanty

--- main.py
+++ main.py
@@ -119,12 +119,7 @@ while running:
         # mini-menu
         res = menu.handle_event(event)
         if res in ("triangle", "tall_rect", "long_rect"):
-            if res == "triangle":
-                uranek.say("postaw generator")
-            elif res == "tall_rect":
-                uranek.say("postaw skrzynke")
-            elif res == "long_rect":
-                uranek.say("postaw kabel")
+                uranek.say("Widzisz ten Generator? Musiz podłączyć go do prądu, aby zasilić pokój")
             player.change_shape(res)
 
         # stawianie obiektów PPM
